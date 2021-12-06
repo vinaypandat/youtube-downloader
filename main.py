e4095438179ce4a410d982d1b97008926e4666ee
vinaypandat <engg.vinaypandey@gmail.com>
2021-12-06 21:05:16 +0000

    Functions added: fetch_audio, fetch_video, extract_streams

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-from pytube import YouTube
 import datetime
 
+from pytube import YouTube
+import ast
 
 # Input for link
 #user_link = input("Please paste your Youtube link here")
-LINK = "https://www.youtube.com/watch?v=QAg1a9RlHPs"
+LINK = "https://www.youtube.com/watch?v=X40Tr3Q-BvE"
 
 yt = YouTube(LINK)
 
-def description(link_yt):
-    print(f"Title: {link_yt.title}"
-          f"\nViews: {link_yt.views}"
-          f"\nLength: {datetime.timedelta(seconds=link_yt.length)}")
-
+# Description function
+def description(yt_link):
+    print(f"Title: {yt_link.title}"
+          f"\nViews: {yt_link.views}"
+          f"\nLength: {datetime.timedelta(seconds=yt_link.length)}")
     return
 
-description(yt)
+# Function to creates dictionary list from fetched streams list
+def extract_streams(streams):
+    extracted_data = streams.strip("[]").replace('<Stream: ','{"').replace(">","}").split(", ")
+    list_of_streams = []
+    for stream in extracted_data:
+        dict = ast.literal_eval(stream.replace(' ', ', "').replace('=', '":'))
+        list_of_streams.append(dict)
+    for stream in list_of_streams:
+        if stream['type'] == 'video':
+            print(f"{list_of_streams.index(stream) + 1}: Quality:{stream['res']}, Type:{stream['type']}, Tag:{stream['itag']}")
+        else:
+            print(f"{list_of_streams.index(stream) + 1}: Quality:{stream['abr']}, Type:{stream['type']}, Tag:{stream['itag']}")
+    return
 
+# Fetch audio streams
+def fetch_audio_streams(yt_link):
+    fetched_streams = str(yt_link.streams.filter(only_audio=True))
+    extract_streams(fetched_streams)
+    return
 
+# Fetch video streams
+def fetch_video_streams(yt_link):
+    fetched_streams = str(yt_link.streams.filter(progressive=True))
+    extract_streams(fetched_streams)
+    return
 
-# Details of link
-# print("Title: ", yt.title, "\n")
-# print("Number of views: ", yt.views, "\n")
-# print("Length of video: ", yt.length, "seconds \n")
-# print("Description: ", yt.description, "\n")
-# print("Ratings: ", yt.rating, "\n")
-#title(yt)
-#print(type(yt.streams))
-#print(yt.streams.get_highest_resolution())
-#print(yt.streams.get_by_itag(yt.streams.get_audio_only().itag))
-#ys = yt.streams.get_by_itag('22')
-#ys.download()
+#description(yt)
+fetch_audio_streams(yt)
+fetch_video_streams(yt)
