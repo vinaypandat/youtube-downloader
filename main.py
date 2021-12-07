33b9f97df6727142b02a8c14c1749267681e412b
vinaypandat <engg.vinaypandey@gmail.com>
2021-12-07 11:30:54 +0000

    Changed functions to work with GUI

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
 import datetime
-
 from pytube import YouTube
 import ast
 
-# Input for link
-#user_link = input("Please paste your Youtube link here")
-LINK = "https://www.youtube.com/watch?v=X40Tr3Q-BvE"
+LINK = "https://wwyoutube.com/watch?v=X40Tr3Q-BvE"
 
 yt = YouTube(LINK)
-
 # Description function
-def description(yt_link):
-    print(f"Title: {yt_link.title}"
-          f"\nViews: {yt_link.views}"
-          f"\nLength: {datetime.timedelta(seconds=yt_link.length)}")
-    return
+def description(yt_link) -> str:
+    desc = (f"Title: {yt_link.title}"
+            f"\nViews: {yt_link.views}"
+            f"\nLength: {datetime.timedelta(seconds=yt_link.length)}")
+    return desc
 
-# Function to creates dictionary list from fetched streams list
-def extract_streams(streams):
-    extracted_data = streams.strip("[]").replace('<Stream: ','{"').replace(">","}").split(", ")
+
+# Function to create dictionary list from fetched streams list
+def extract_streams(streams, stream_type)->list:
+    extracted_data = streams.strip("[]").replace('<Stream: ', '{"').replace(">", "}").split(", ")
     list_of_streams = []
     for stream in extracted_data:
-        dict = ast.literal_eval(stream.replace(' ', ', "').replace('=', '":'))
-        list_of_streams.append(dict)
+        dict_stream = ast.literal_eval(stream.replace(' ', ', "').replace('=', '":'))
+        list_of_streams.append(dict_stream)
+        audio_list = []
+        video_list = []
     for stream in list_of_streams:
         if stream['type'] == 'video':
-            print(f"{list_of_streams.index(stream) + 1}: Quality:{stream['res']}, Type:{stream['type']}, Tag:{stream['itag']}")
+            video_list.append(
+                f"Quality:{stream['res']}, Type:{stream['type']}, Tag:{stream['itag']}")
         else:
-            print(f"{list_of_streams.index(stream) + 1}: Quality:{stream['abr']}, Type:{stream['type']}, Tag:{stream['itag']}")
-    return
+            audio_list.append(f"Quality:{stream['abr']}, Type:{stream['type']}, Tag:{stream['itag']}")
+    if stream_type == "audio":
+        return audio_list
+    if stream_type == "video":
+        return video_list
+
 
 # Fetch audio streams
-def fetch_audio_streams(yt_link):
+def fetch_audio_streams(yt_link)->list:
     fetched_streams = str(yt_link.streams.filter(only_audio=True))
-    extract_streams(fetched_streams)
-    return
+    audio_list = extract_streams(fetched_streams, "audio")
+    return audio_list
+
 
 # Fetch video streams
-def fetch_video_streams(yt_link):
+def fetch_video_streams(yt_link)->list:
     fetched_streams = str(yt_link.streams.filter(progressive=True))
-    extract_streams(fetched_streams)
+    video_list = extract_streams(fetched_streams, "video")
+    return video_list
+
+
+# Download function
+
+def download_stream(tag, yt_link):
+    yt_stream = yt_link.streams.get_by_itag(tag)
+
+
+def main():
+
+    description(yt)
+    # choice = input("Select the format you want to download:\n1.Audio\n2.Video")
     return
 
-#description(yt)
-fetch_audio_streams(yt)
-fetch_video_streams(yt)
+#fetch_audio_streams(yt)
